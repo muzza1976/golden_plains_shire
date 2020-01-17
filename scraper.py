233b92791bc49a276d38e87eea7a885d424c1678
muzza1976 <murray.buchanan@gmail.com>
2020-01-17 20:21:35 +1100

    Initial Scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,37 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+applications_url = 'https://www.goldenplains.vic.gov.au/residents/my-home/planning-applications-currently-advertised'
+html = scraperwiki.scrape(applications_url)
+page = BeautifulSoup(html, 'html.parser')
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+for div in page.find_all('div', class_='accordion'):
+  council_reference = div.find('h4').getText().split()[1]
+  for div2 in div.find_all('div', class_='pan-content'):
+    if div2.find('span', string='Description:'):
+      description = div2.find('p').get_text()
+    elif div2.find('span', string='Application Location:'):
+      address = div2.find('p').getText().split(', ', 1)[1] + ' VIC'
+    elif div2.find('span', string='Advertising Starts:'):
+      start_date = div2.find('span', class_='date-display-single')
+      if start_date:
+        on_notice_from = datetime.strptime(start_date.getText(), '%d/%m/%Y').date().isoformat()
+    elif div2.find('span', string='The Planning Department will not make a decision before:'):
+      end_date = div2.find('span', class_='date-display-single')
+      if end_date:
+        on_notice_to = datetime.strptime(end_date.getText(), '%d/%m/%Y').date().isoformat()
+
+  record = {
+      'council_reference': council_reference,
+      'address': address,
+      'description' : description,
+      'on_notice_from' : on_notice_from,
+      'on_notice_to' : on_notice_to,
+      'info_url': applications_url,
+      'comment_url': applications_url,
+      'date_scraped': date.today().isoformat(),
+    }
+
+  scraperwiki.sqlite.save(unique_keys=['council_reference'], data=record)
